6c5d6ba86afd7ef7f3e7d0e9208e49279cb7f2c1
Andrey <erkcasb@gmail.com>
2020-12-04 15:44:31 +0500

    Задание 1 дополнено

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         int c = calc.divide.apply(a, b);
         calc.println.accept(c);
         calc.println.accept(calc.abs.apply(0));
-        calc.println.accept(calc.abs.apply(-5));
-        calc.println.accept(calc.abs.apply(5));
+        calc.println.accept(calc.abs.apply(-7));
+        calc.println.accept(calc.abs.apply(7));
         calc.println.accept(calc.abs.apply(Integer.MIN_VALUE));
         calc.println.accept(calc.abs.apply(Integer.MAX_VALUE));
     }
